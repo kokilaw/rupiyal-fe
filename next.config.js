ef2534590455eeb85169474151fd726c1f8f5bf8
kokilaw <kokilapc@gmail.com>
2024-04-30 04:25:06 +0530

    nextjs config updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  serverActions: {
+    allowedOrigins: [
+      'f1dc190e-0883-4eeb-8830-209c3e4c3762.e1-us-cdp-2.choreoapps.dev',
+    ],
+  },
 };
 
 export default nextConfig;
