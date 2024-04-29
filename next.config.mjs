8e3302e1ae34e88aa17cace6935dfcf81339fe85
kokilaw <kokilapc@gmail.com>
2024-04-30 04:43:37 +0530

    allowedForwardedHosts added

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
         'f1dc190e-0883-4eeb-8830-209c3e4c3762.e1-us-cdp-2.choreoapps.dev',
         '3f5bd791-ba97-4f86-92e7-ad0e0ac4467e.e1-us-cdp-2.choreoapps.dev',
       ],
+      allowedForwardedHosts: [
+        'rupiyalfe-671681752-choreo-internal.dp-development-client-39382-3179226743:8080',
+      ],
     },
   },
 };
