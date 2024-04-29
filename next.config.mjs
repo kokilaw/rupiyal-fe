648532a5189eff69c5b613786f09c3f8cb8da180
kokilaw <kokilapc@gmail.com>
2024-04-30 04:29:32 +0530

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
   serverActions: {
     allowedOrigins: [
       'f1dc190e-0883-4eeb-8830-209c3e4c3762.e1-us-cdp-2.choreoapps.dev',
+      '3f5bd791-ba97-4f86-92e7-ad0e0ac4467e.e1-us-cdp-2.choreoapps.dev'
     ],
   },
 };
