cde4c8b32cb2e84b6e6a719a1d24a3098a015b5c
kokilaw <kokilapc@gmail.com>
2024-04-30 05:05:21 +0530

    Landing page updated

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import { DEFAULT_BANK } from '@/utils/Constants';
 
 export default function Page({ params = {} }) {
   return (
-    <div className="relative isolate px-6 lg:px-8">
-      <div className="mx-auto max-w-5xl py-32 sm:py-32 lg:py-40">
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
@@ -18,6 +18,14 @@ export default function Page({ params = {} }) {
               intuitive currency converter for seamless calculations, all on
               lkr.exchange!
             </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="/currency-converter/buying-rate-usd?bankCode=BOC"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Launch Converter
+              </a>
+            </div>
           </div>
         </div>
       </div>
